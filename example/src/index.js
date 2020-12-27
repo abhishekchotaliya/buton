2267abf776f94bb27d55e692b233638fcdcf2053
aBi <abi@aBis-MacBook-Air.local>
2020-12-27 13:51:27 +0530

    cleanup

--- example/src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<div className="my-test-bg"><App /></div>, document.getElementById('root'))
